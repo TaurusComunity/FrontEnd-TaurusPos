00bf94f32d22f93462bebf468e0b1792a8dc61ff
JhoannS <sebastianzamudio2004@gmail.com>
2024-12-11 15:44:43 -0500

    Inicio de App, se crea vista de login sin animaciones, 11 DIC 2024, 15:41

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            rojo: '#FF3131',
+            rojo_secundario: '#FD5D5D',
+            blanco: '#FFFFFF',
+            gris: '#A1A1A1',
+            negro_fuerte: '#000000',
+            negro_suave: '#1E1E1E',
+            azul_oscuro: '#1A2130',
+            verde: '#81A263',
+          }
+        }
+      }
+    }
